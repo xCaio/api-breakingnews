041b8a90a69b2e5d3c88bda5dbde90057bb33620
Caio Santos <mrlose22@gmail.com>
2024-08-31 12:24:24 -0300

    refactor: added the software architecture

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
 const express = require('express')
+const userRoute = require('./src/routes/user.route')
+
 const app = express()
 
-    //ROTA
-        // Method HTTP - CRUD (CREATE, READ, UPDATE, DELETE)
-            // GET - Pega uma info
-            // POST - Cria uma info
-            // PUT - Altera toda a info
-            // PATH - Altera parte da info
-            // DELETE - Apaga a info
-
-        // Name - Um identificador da rota
-
-        // Function (Callback) - Responsável por executar algum comando
-app.get('/soma', (req, res) => {
-    const soma = 100 + 1;
-
-    res.send({soma: soma})
-})
+app.use('/soma', userRoute)
 
 app.listen(3000)
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,6 @@
+const soma = (req, res) => {
+    const soma = 100 + 1;
+    res.json(soma)
+}
+
+module.exports = { soma }
